80bcbe31cc9749a4b9614bef3b2b2ec12f119daa
Persi87 <simon_black87@hotmail.co.uk>
2021-08-30 23:03:04 +0100

    commit tree b94cb791c95de5af0f346824b16498fdc55d7bd5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+
+class School 
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+            @roster[grade] ||= []          #roster[grade] is true (has a value), add student, or (if not,) create new array
+            @roster[grade] << student_name      # then add student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, names|
+            names.sort!
+        end
+    end
+
+end
